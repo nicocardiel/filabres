fc1b76a6896b7073447fc491ee6c13706596b1b8
Nicolás Cardiel <cardiel@ucm.es>
2020-03-05 17:03:08 +0100

    Test the use of Loader=yaml.BaseLoader in yaml.load()

--- filabres/load_setup.py
+++ filabres/load_setup.py
@@ -35,7 +35,7 @@ def load_setup(setupfile, verbose=False):
     if setupfile is None:
         setupfile = 'setup_filabres.yaml'
     with open(setupfile) as yamlfile:
-        setupdata = yaml.load(yamlfile)
+        setupdata = yaml.load(yamlfile, Loader=yaml.BaseLoader)
 
     for kwd in ['instrument', 'datadir', 'image_corrections_file']:
         if kwd not in setupdata:
